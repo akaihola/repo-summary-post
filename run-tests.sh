c7a0e89fd63f35cec4f36b6ab36b6ad222bc4221
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-08-02 19:29:33 +0300

    build: test run the script in `run-tests.sh`

--- run-tests.sh
+++ run-tests.sh
@@ -10,9 +10,9 @@ ensure() { command -v $1 >/dev/null || ${PIP} install -q $1; }
 ensure pytest
 
 errors=0
-#${VENV}/bin/darkgray_collect_contributors \
-#  --repo akaihola/darkgray-dev-tools \
-#  || errors=$?
+export INPUT_GITHUB_TOKEN=$(secret-tool lookup service gh:github.com)
+export INPUT_REPO_NAME=akaihola/darker
+${VENV}/bin/python summarize_prs.py || errors=$?
 ${VENV}/bin/pytest --quiet || errors=$?
 
 exit $errors
